263f03f33deb5776367354778d20afa6b423f0d7
Ubbemir <ubbermir@example.com>
2025-04-03 03:03:25 +0200

    Removed typo in error string

--- src/error.rs
+++ src/error.rs
@@ -8,7 +8,7 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             Error::ParsingError(e) => write!(f, "Parsing error: {}", e),
-            Error::FormattingError(e) => write!(f, "Formatting error error: {}", e)
+            Error::FormattingError(e) => write!(f, "Formatting error: {}", e)
         }
     }
 }
