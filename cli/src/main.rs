17c7d1cc826f6e99e9ffdc62d0cf722668ea485b
Ubbemir <ubbemir@example.com>
2025-07-07 21:37:55 +0200

    Added a minify option to the CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,23 +9,30 @@ struct Args {
     /// Read CFG from a file instead of stdin
     #[arg(short, long)]
     input: Option<String>,
+
+    /// Minify instead of prettifying
+    #[arg(short, long)]
+    minify: bool,
 }
 fn main() {
     let args = Args::parse();
 
+    let content: String;
     if let Some(cfg_file) = args.input {
-        let content = fs::read_to_string(&cfg_file)
+        content = fs::read_to_string(&cfg_file)
             .expect(&format!("Could not read file: {}", cfg_file));
-
-        println!("{}", formatters::prettify(&content).unwrap());
     } else {
         let mut input = String::new();
         io::stdin().read_to_string(&mut input).expect("Failed to read from stdin");
-        println!("{}", formatters::prettify(&input).unwrap());
+        content = input;
+    }
+
+    if args.minify {
+        println!("{}", formatters::minify(&content).unwrap());
+    } else {
+        println!("{}", formatters::prettify(&content).unwrap());
     }
 }
 
 // TODO: Make a separate function for taking an Args as input, reading input data based on how user chose to and returning it as a String. Make it purely functional with mockup IO possible to it is
 // unit testable.
-
-// TODO: Make an option for either prettifying or minifying the input
